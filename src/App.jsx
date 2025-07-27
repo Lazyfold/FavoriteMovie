f66bb56225853c1ba1b69198a24992d2777ca0d5
Lazyfold <109660348+Lazyfold@users.noreply.github.com>
2025-07-27 13:28:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import "./css/App.css";
 import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { MovieProvider } from "./context/MovieContext";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <MovieProvider>
-      <NavBar />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </main>
+      <HashRouter>
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </main>
+      </HashRouter>
     </MovieProvider>
   );
 }
