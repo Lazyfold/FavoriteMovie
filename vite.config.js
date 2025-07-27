bf453dbca606c844ef1a0b6f372212562e71f411
Lazyfold <109660348+Lazyfold@users.noreply.github.com>
2025-07-27 13:27:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/FavoriteMovie//',
+  base: '/FavoriteMovie/',
   plugins: [react()],
 })
